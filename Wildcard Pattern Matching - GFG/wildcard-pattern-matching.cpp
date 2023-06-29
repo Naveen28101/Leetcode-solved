2e91273ad16886cce1f6c6649a66c4765af9f2c0
Naveen Yadav <110476246+Naveen28101@users.noreply.github.com>
2023-06-29 12:19:13 +0530

    Added solution - LeetHub

--- Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.cpp
@@ -26,13 +26,40 @@ class Solution{
         return 0;
     }
     
+        // vector<vector<int>> dp(n,vector<int>(m,-1)); 
+        // return f(n-1,m-1,pattern,str,dp);
+        
+    bool isAllStars(string p,int i){
+        for(int j=1;j<=i;j++){
+            if(p[j-1]!='*') return false;
+        }
+        return true;
+    }
+    
   public:
 /*You are required to complete this method*/
     int wildCard(string pattern,string str){
         int n=pattern.length();
         int m=str.length();
-        vector<vector<int>> dp(n,vector<int>(m,-1)); 
-        return f(n-1,m-1,pattern,str,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        dp[0][0]=1;
+        for(int i=0;i<=n;i++){
+            if(isAllStars(pattern,i)){
+                dp[i][0]=1;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(pattern[i-1]==str[j-1] || pattern[i-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else if(pattern[i-1]=='*'){
+                    dp[i][j]=dp[i-1][j] | dp[i][j-1];
+                }
+                else dp[i][j]=0;
+            }
+        }
+        return dp[n][m];
     }
 };
 
